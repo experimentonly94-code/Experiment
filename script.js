d08b5548efb6a4394c7e42a37cce356caca861c7
experimentonly94-code <experimentonly94@gmail.com>
2025-10-24 22:12:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle menu saat tombol hamburger diklik
-const hamburgerBtn = document.getElementById("hamburgerBtn");
-const navLinks = document.getElementById("navLinks");
-
-hamburgerBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
